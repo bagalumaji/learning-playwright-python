251a8a68a4f51941e2f7e53508dbac1b00985242
DESKTOP-2HHA8QU\sayaji-sharu <bagalumaji@gmail.com>
2025-07-07 17:07:06 +0530

    git ignore

--- window_handling.py
+++ window_handling.py
@@ -1,6 +1,26 @@
-from playwright.sync_api import Page
+from playwright.sync_api import sync_playwright, Page
 
 
-def test_verify_window_handling(page:Page):
+def test_verify_window_handling():
+    playwright = sync_playwright().start()
+
+    browser = playwright.chromium.launch(headless=False,channel="chrome")
+    context = browser.new_context()
+    page = context.new_page()
     page.goto("https://www.lambdatest.com/selenium-playground/window-popup-modal-demo")
     print(page.title())
+
+    page.locator("//a[normalize-space()='Follow On Twitter']").wait_for(state="visible", timeout=10000)
+
+    page.locator("//a[normalize-space()='Follow On Twitter']").click(force=True)
+
+    new_page = context.wait_for_event("page")
+    new_page.wait_for_load_state()
+    print(new_page.title())
+
+    new_page.close()
+    page.close()
+    context.close()
+    browser.close()
+    playwright.stop()
+
