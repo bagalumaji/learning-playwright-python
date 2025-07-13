03be6df6490d4510b548d1c06de3d6fd1a958ee1
umaji bagal <bagalumaji@gmail.com>
2025-07-13 11:10:04 +0530

    window popup

--- window_handling.py
+++ window_handling.py
@@ -1,7 +1,8 @@
 from playwright.sync_api import sync_playwright, Page
+from pytest_playwright.pytest_playwright import browser
 
 
-def test_verify_window_handling():
+def te1st_verify_window_handling():
     playwright = sync_playwright().start()
 
     browser = playwright.chromium.launch(headless=False,channel="chrome")
@@ -24,3 +25,22 @@ def test_verify_window_handling():
     browser.close()
     playwright.stop()
 
+
+def test_window_handling_demo_1():
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page_1 = context.new_page()
+    page_1.goto("http://testautomationpractice.blogspot.com/")
+
+    with context.expect_page() as page_promise:
+        page_1.locator("id=PopUp").click()
+
+    page_2 = page_promise.value
+    print(page_2.title())
+    print(page_2.url)
+
+    page_2.wait_for_timeout(10000)
+    page_2.close()
+    page_1.close()
+    playwright.stop()
