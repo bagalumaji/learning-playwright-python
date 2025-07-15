fb1c0e723d0572007b74c7e43a56a7d16c2f1a88
Umaji Bagal <bagalumaji@gmail.com>
2025-07-15 12:15:42 +0530

    alert

--- alert.py
+++ alert.py
@@ -1,6 +1,17 @@
-from playwright.sync_api import Page
+from playwright.sync_api import sync_playwright
 
+def test_alert_demo():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("http://testautomationpractice.blogspot.com/")
 
-def test_alert_demo(page:Page):
-    page.goto("")
+        #need to update --> now its not working
+        with page.expect_dialog() as dialog_info:
+            page.click("#alertBtn")
 
+        dialog = dialog_info.value
+        print("Alert message:", dialog.message)
+        dialog.accept()
+
+        browser.close()
